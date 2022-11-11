9780252036aa4738a513fefe43af09e1d7e9878b
ivanturbin <ivanturbin2004@gmail.com>
2022-11-11 20:09:52 +0300

    ы

--- homeworks/task_2/src/Task_2.java
+++ homeworks/task_2/src/Task_2.java
@@ -1,12 +1,8 @@
-import java.lang.Math;
-
 public class Task_2 implements Task_2_base {
     @Override
     public int subtask_1_while(int num) {
         // Найти максимальное число, являющееся полным квадратом,
         // не превосходящее заданное натуральное num
-        //------------------------------------------------------
-
         int interNumber = 0, finalNumber = 0;
         while (interNumber <= num){
             if(interNumber*interNumber <= num)
@@ -24,8 +20,6 @@ public class Task_2 implements Task_2_base {
         // p(0) = 1
         // p(k) = 2 * p(k - 1) + 6, k > 0
         //Найти элемент последовательности с номером num
-        //--------------------------------------------------------------------
-
         int interNumber = 1, lastMeaning = 1, interMeaning = 0;
         if(num == 0)
             return 1;
@@ -42,8 +36,6 @@ public class Task_2 implements Task_2_base {
     @Override
     public boolean subtask_3_while(int num, int base) {
         // Проверить, является ли число num натуральной степенью числа base
-        //-----------------------------------------------------------------
-
         int degree = base;
         while(degree < num)
             degree *= base;
@@ -56,19 +48,15 @@ public class Task_2 implements Task_2_base {
         // вычесть 1
         // поделить на 2
         // число start можно превратить в end. Гарантируется, что start >= end >= 1
-        //-------------------------------------------------------------------------
-
         int count = 0;
         while(start > end){
             if(start%2 != 0 || start-end <= 2){
                 start -= 1;
-                count++;
             } else {
                 start /= 2;
-                count++;
             }
+            count++;
         }
         return count;
     }
 }
-
