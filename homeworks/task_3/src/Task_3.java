578c49980e1c3c02875315e05b57dcc4f857769b
ivanturbin <ivanturbin2004@gmail.com>
2022-11-11 20:11:30 +0300

    Update Task_3.java
    -

--- homeworks/task_3/src/Task_3.java
+++ homeworks/task_3/src/Task_3.java
@@ -1,26 +1,22 @@
 public class Task_3 implements Task_3_base {
-    @Override
     public int subtask_1_for(int n1, int n2, int a, int b) {
         // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
-        //------------------------------------------------------
-
-        int count = 0;
+        int x = 0;
         if(n1 < n2){
-            for(; n1 <= n2; n1++){
-                if(n1%a == 0 && n1%b !=0)
-                    count++;
+            for(;n1 <= n2; n1++){
+                if(n1 % a == 0 && n1 % b !=0)
+                    x++;
             }
         } else {
-            for(; n2 <= n1; n2++){
-                if(n2%a == 0 && n2%b !=0)
-                    count++;
+            for(;n2 <= n1; n2++){
+                if(n2 % a == 0 && n2 % b != 0)
+                    x++;
             }
         }
-        return count;
+        return x;
     }
 
-    @Override
     public int subtask_2_for(int num) {
         // Последовательность чисел строится следующим образом:
         // сначала идет одна единица,
@@ -31,47 +27,38 @@ public class Task_3 implements Task_3_base {
         // ...
         // Найти, какое число будет находиться в этой последовательности
         // на позиции num
-        //--------------------------------------------------------------
-
-        int count = 0, finalNumber = 0;
-        for(int i = 1; i <= num; i++){
-            count += i;
-            if(count >= num){
-                finalNumber = i;
-                break;
-            }
+        int x = 0, y = 0, z = 1;
+        while (y < num){
+            y += z;
+            x = z;
+            z = z + 1;
         }
-        return finalNumber;
+        return x;
     }
 
-    @Override
     public int subtask_3_for(int num, int d, int cnt) {
         // Дана последовательность
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-
-        int lastMeaning = num, nextMeaning, sum = num;
-        for(int i = 1; i < cnt; i++){
-            nextMeaning = lastMeaning*d+1;
-            sum += nextMeaning;
-            lastMeaning = nextMeaning;
+        int x = num, y = 0;
+        for (int z = 0; z < cnt; z++){
+            y += x;
+            x = x * d + 1;
         }
-        return sum;
+        return y;
     }
 
-    @Override
     public int subtask_4_for(int n) {
         // Вычислить сумму
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        //-----------------------------------------------
-        int sum = 0, interMeaning = 1;
-        for(int i = 1; i <= n; i++){
-            interMeaning *= i;
-            sum += interMeaning;
+        int x = 1, amount = 0;
+        for(int y = 1; y <= n; y++) {
+            x *= y;
+            amount += x;
         }
-        return sum;
+        return amount;
     }
 }
