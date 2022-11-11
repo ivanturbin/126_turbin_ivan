7931fd1e06c833cfc6f4d3863a6bce924f35a8f7
ivanturbin <ivanturbin2004@gmail.com>
2022-11-11 20:21:27 +0300

    0

--- homeworks/task_4/src/Task_4.java
+++ homeworks/task_4/src/Task_4.java
@@ -1,104 +1,85 @@
 public class Task_4 implements Task_4_base {
-    @Override
     public int[] subtask_1_arrays(int size, int a0, int d) {
-            int newArray[] = new int[size];
-            newArray[0]=a0;
-            for (int i = 1; i < size; i++){
-                newArray[i]=newArray[i-1]+d;
-            }
-            return newArray;
+        // сгенерировать и вернуть массив размера size, содержащий элементы
+        // арифметической прогрессии с первым членом a0 и разностью d
+        int[] array_1 = new int[size];
+        array_1[0] = a0;
+        for (int y = 1; y < size; y++) {
+            array_1[y] = array_1[y - 1] + d;
+        }
+        return array_1;
     }
 
-    @Override
     public int[] subtask_2_arrays(int size) {
-        int newArray[] = new int[size];
-        int sum = 2;
-        for(int i = 0; i < size; i++)
+        // сгенерировать и вернуть массив размера size, первые два элемента
+        // которого равны единице, а каждый следующий - сумме всех предыдущих
+        int array_2[] = new int[size];
+        int amount = 2;
+        for(int y = 0; y < size; y++)
         {
-            if (i <= 1) {
-                newArray[i] = 1;
+            if (y <= 1) {
+                array_2[y] = 1;
             }
             else {
-                newArray[i] = sum;
-                sum = sum + newArray[i];
+                array_2[y] = amount;
+                amount += array_2[y];
             }
         }
-        return newArray;
+        return array_2;
+
+
     }
 
-    @Override
     public int[] subtask_3_arrays(int size) {
-        int newArray[] = new int[size];
-        for(int i = 0; i < size; i++)
-        {
-            if (i == 0) {
-                newArray[i] = 0;
-            }
-            else {
-                if (i == 1){
-                    newArray[i] = 1;
-                }
-                else {
-                    newArray[i] = newArray[i - 1] + newArray[i - 2];
-                }
-            }
+        // сгенерировать и вернуть массив размера size, содержащий первые
+        // size чисел последовательности фибоначчи.
+        // https://ru.wikipedia.org/wiki/Числа_Фибоначчи
+        int[] array_3 = new int[size];
+        for (int y = 0; y < size; y++) {
+            if (y == 0) array_3[y] = 0;
+            if (y == 1 || y == 2) array_3[y] = 1;
+            if (y > 2) array_3[y] = array_3[y - 2] + array_3[y - 1];
         }
-        return newArray;
+        return array_3;
+
     }
 
-    @Override
     public int subtask_4_arrays(int[] data) {
+        // Для данного массива вычислить максимальный элемент
         int max = data[0];
-        for(int i = 0; i < data.length; i++) {
-            if (data[i]>max){
-                max=data[i];
+        for (int y = 0; y < data.length; y++) {
+            if (data[y] > max){
+                max = data[y];
             }
         }
         return max;
     }
 
-    @Override
     public int subtask_5_arrays(int[] data, int k) {
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < data.length; i++) {
-            if ((data[i]>max) && (data[i] % k == 0)){
-                max=data[i];
-            }
+        // Для данного массива вычислить максимальный элемент
+        // кратный k. Гарантируется, что как минумум один такой элемент
+        // в массиве есть
+        int max = data[1];
+        for (int y = 0; y < data.length; y++) {
+            if (data[y] > max && data[y] % k == 0)
+                max = data[y];
         }
         return max;
     }
 
-    public static void Sort(int[] a){
-        boolean sorted = false;
-        int temp;
-        while(!sorted) {
-            sorted = true;
-            for (int i = 0; i < a.length - 1; i++) {
-                if (a[i] > a[i+1]) {
-                    temp = a[i];
-                    a[i] = a[i+1];
-                    a[i+1] = temp;
-                    sorted = false;
-                }
-            }
-        }
-    }
-
-    @Override
     public int[] subtask_6_arrays(int[] arr1, int[] arr2) {
-        int[] arr3 = new int[arr1.length + arr2.length];
-        for(int i = 0; i < arr1.length; i++) {
-            arr3[i]=arr1[i];
-        }
-        for(int i = arr1.length; i < arr3.length; i++) {
-            arr3[i]=arr2[i-arr1.length];
-        }
-        Sort(arr3);
-        return arr3;
+        // Даны два массива arr1, arr2.
+        // Произвести слияние данных массивов в один отсортированный
+        // по возрастанию массив.
+        return null;
     }
 
-    @Override
     public int[] subtask_7_arrays(int[] arr1, int[] arr2) {
+        // Даны два отсортированных по возрастанию массива arr1, arr2.
+        // Произвести слияние данных массивов в один также отсортированный
+        // по возрастанию массив.
+        // Используйте алгоритм, время работы которого будет пропорционально сумме
+        // размеров arr1 и arr2, а не их произведению
         return null;
     }
 }
